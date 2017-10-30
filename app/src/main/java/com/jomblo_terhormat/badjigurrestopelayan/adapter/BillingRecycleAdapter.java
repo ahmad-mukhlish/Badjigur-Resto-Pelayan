07fc304a2199c87baf4f95f9ec255c03ffc6d381
Ahmad Mukhlis Saputra <ahmad_mukhlish_s@yahoo.co.id>
2017-10-30 21:28:02 +0700

    fix billing layout without total

--- app/src/main/java/com/jomblo_terhormat/badjigurrestopelayan/adapter/BillingRecycleAdapter.java
+++ app/src/main/java/com/jomblo_terhormat/badjigurrestopelayan/adapter/BillingRecycleAdapter.java
@@ -5,12 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jomblo_terhormat.badjigurrestopelayan.R;
 import com.jomblo_terhormat.badjigurrestopelayan.entity.Produk;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -38,13 +36,7 @@ public class BillingRecycleAdapter extends RecyclerView.Adapter<BillingRecycleAd
     public void onBindViewHolder(final BillingViewHolder holder, int position) {
         final Produk currentProduk = mProduks.get(position);
 
-        Picasso.with(mContext).
-                load(Produk.BASE_PATH + currentProduk.getPath()).
-                into(holder.mGambar);
-
-
         holder.mJudul.setText(currentProduk.getNama());
-        holder.mTag.setText(currentProduk.getTag());
         holder.mPrice.setText("Rp. " + (currentProduk.getHarga_jual() * currentProduk.getmQty()));
         holder.mQty.setText(currentProduk.getmQty() + "");
 
@@ -59,16 +51,13 @@ public class BillingRecycleAdapter extends RecyclerView.Adapter<BillingRecycleAd
 
     class BillingViewHolder extends RecyclerView.ViewHolder {
 
-        ImageView mGambar;
-        TextView mJudul, mTag, mPrice, mQty;
+        TextView mJudul, mPrice, mQty;
         View mItemView;
 
 
         BillingViewHolder(View itemView) {
             super(itemView);
-            mGambar = itemView.findViewById(R.id.gambar);
             mJudul = itemView.findViewById(R.id.judul);
-            mTag = itemView.findViewById(R.id.tag);
             mPrice = itemView.findViewById(R.id.price);
             mQty = itemView.findViewById(R.id.qty);
             mItemView = itemView;
